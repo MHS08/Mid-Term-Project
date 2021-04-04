7dcc1c61fd86cf06f154ed59cdf888ee21b10358
Muhammad Hamza Saeed <81794794+MHS08@users.noreply.github.com>
2021-04-05 00:26:54 +0500

    Mid term project

---
+++ GUI_Prototype/src/gui_prototype/GUI_Prototype.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui_prototype;
+
+/**
+ *
+ * @author MHS
+ */
+public class GUI_Prototype {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        HomePage form1 = new HomePage();
+        form1.setVisible(true);
+    }
+    
+}
